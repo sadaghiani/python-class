3a311949696ccdea670e3c324da0b4aceaf8d0bc
reza sadaghiani <45229121+sadaghiani@users.noreply.github.com>
2020-04-10 10:34:24 +0430

    Update exercise01.py

--- exercises/exercise01.py
+++ exercises/exercise01.py
@@ -18,6 +18,51 @@ print ("Count of all characters in word is :\n " +  str(characterFrequency))
 
 -----------------------------
 #2. Write a Python program to remove the characters which have odd index values of a given string
+
+def oddValuesString(str):
+  result = "" 
+  for i in range(len(str)):
+    if i % 2 == 0:
+      result += str[i]
+  return result
+
+print(oddValuesString('python'))
+
+-----------------------------
 #3. Write a Python program to count the occurrences of each word in a given sentence
+import string 
+
+text = "Mango- banana, apple! pear Mango!"  
+
+dic = {}
+
+text = text.translate(text.maketrans("", "", string.punctuation))
+text = text.strip() 
+text = text.lower() 
+text = text.split(" ") 
+
+for word in text: 
+  if word in dic: 
+    dic[word] = dic[word] + 1
+  else: 
+    dic[word] = 1
+
+print(dic)
+
+-----------------------------
 #4. Write a Python program that accepts a comma separated sequence of words as input and prints the unique words in sorted form
- 
+
+s = input()
+words = [word for word in s.split(",")]
+print( ",".join(sorted(list(set(words)))))
+
+-----------------------------
+#5. isPrime
+
+for val in range(11, 25): 
+   if val > 1: 
+       for n in range(2, val): 
+           if (val % n) == 0: 
+               break
+       else: 
+           print(val)
